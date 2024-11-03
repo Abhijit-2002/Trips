ea45aa7bc48a4633acd738ab79c959658e49993a
Abhijit Chakraborty <115333386+Abhijit-2002@users.noreply.github.com>
2024-11-03 12:22:19 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button } from "@/components/ui/button.jsx";
+// import { Button } from "./components/ui/button.jsx";
+import { Button } from "./ui/button";
 
 const menuItems = [
   {
